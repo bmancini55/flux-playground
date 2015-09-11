91625323ad4ce19c4bd1c05f423e34664e6486c9
Brian Mancini <bmancini@gmail.com>
2015-09-11 16:27:23 -0400

    Initial bootstrapping of jade templates

--- client/js/app.js
+++ client/js/app.js
@@ -3,3 +3,10 @@ let React          = require('react');
 let RestaurantList = require('./components/restaurant-list.jsx');
 
 React.render(<RestaurantList />, document.getElementById('app'));
+
+
+/**
+let restaurantList2 = require('./components/restaurant-list.jade');
+let app2 = document.getElementById('app2');
+app2.innerHTML = restaurantList2({ title: 'Hello', restaurants: [ { name: 'betos' }] });
+**/
